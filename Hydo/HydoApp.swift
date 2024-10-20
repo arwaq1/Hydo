66a72bdac80275991d9cb96798c4651456886d4c
arwaq1 <arwaalqhtani52@gmail.com>
2024-10-20 11:07:52 +0300

    Initial Commit

---
+++ Hydo/HydoApp.swift
@@ -0,0 +1,17 @@
+//
+//  HydoApp.swift
+//  Hydo
+//
+//  Created by Arwa Alqhtani on 17/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct HydoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
